88b39c65bc63c24a2910ab941fecf51be074f6af
macmade <macmade@xs-labs.com>
2015-06-16 20:09:52 +0200

    Added dummy source fles, unit tests and Xcode project.

---
+++ Semaphore/Source/Semaphore.m
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2014 Jean-David Gadina - www-xs-labs.com
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ ******************************************************************************/
+
+#import "Semaphore.h"
+
+#ifndef __has_feature
+#define __has_feature( _x_ )    0
+#endif
+
+#if __has_feature( objc_arc )
+#define SEMAPHORE_ARC           1
+#else
+#define SEMAPHORE_ARC           0
+#endif
+
+#if defined( __IPHONE_OS_VERSION_MIN_REQUIRED ) && __IPHONE_OS_VERSION_MIN_REQUIRED
+#define SEMAPHORE_IOS           1
+#else
+#define SEMAPHORE_IOS           0
+#endif
+
+@interface Semaphore()
+
+
+
+@end
+
+@implementation Semaphore
+
+
+
+@end
